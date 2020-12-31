0a4b2d3aff7c6e5b6c4016432f1a9c15940ca63b
Luke Longren <lcubelongren@gmail.com>
2020-12-31 02:49:52 +0100

    added homepage design animation

--- pages/homepage/views.py
+++ pages/homepage/views.py
@@ -1,9 +1,24 @@
 from django.http import HttpResponse
 from django.template import loader
 
+import numpy as np
+import matplotlib.pyplot as plt
+
 
 def index(request):
+    plt.switch_backend('agg')
+    
+    t = np.linspace(0, np.pi, 1000)
+    fig = plt.figure(figsize=(6,4), dpi=100)
+    plt.plot(np.sin(t), np.cos(t), c='k')
+    plt.plot(-np.sin(t), np.cos(t), c='k')
+    plt.axis('off')
+    
+    plt.savefig('static/images/test/thing.png', transparent=True)
+
+    # -----
     template = loader.get_template('homepage/homepage.html')
-    context = {
-	}
+    context = {}
     return HttpResponse(template.render(context, request))
+    
+    
