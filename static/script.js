f42ce642b828f2ef3cbfabb06c1d3aabd0ce5718
Luke Longren <lcubelongren@gmail.com>
2021-01-20 22:49:10 +0100

    added to projects page, changed font family

--- static/script.js
+++ static/script.js
@@ -1,1 +1,15 @@
-/* currently empty */
+
+/* project function */
+/* this opens more information for an individual project id */
+
+function projectFunction(id,arrow_id) {
+  var x = document.getElementById(id);
+  var arrow = document.getElementById(arrow_id);
+  if (x.style.display === "block") {
+    x.style.display = "none";
+	arrow.style.transform = "rotate(135deg) translate(0px,-10px)";
+  } else {
+    x.style.display = "block";
+	arrow.style.transform = "rotate(45deg) translate(5px,5px)";
+  }
+}
