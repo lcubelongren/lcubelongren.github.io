8f594760756ea9f00f6791b4f72f4125070b3907
Luke Longren <lcubelongren@gmail.com>
2021-02-04 22:33:53 +0100

    progress on projects page

--- static/script.js
+++ static/script.js
@@ -15,14 +15,17 @@ function projectsectionFunction(id,butt_id) {
   }
 }
 
-function projectFunction(id,arrow_id) {
+function projectFunction(id,text_id,arrow_id) {
   var x = document.getElementById(id);
+  var xtext = document.getElementById(text_id);
   var arrow = document.getElementById(arrow_id);
   if (x.style.display === "block") {
     x.style.display = "none";
+	xtext.style.display = "none";
 	arrow.style.transform = "rotate(135deg) translate(0px,-10px)";
   } else {
     x.style.display = "block";
+	xtext.style.display = "block";
 	arrow.style.transform = "rotate(45deg) translate(5px,5px)";
   }
 }
